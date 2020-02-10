5ebec7f474249605fb20d03fd15188b8be59e2c5
Unknown <simon.kallberg1@gmail.com>
2020-02-10 11:48:12 +0100

    Added gitignore and stuff

--- app.py
+++ app.py
@@ -3,4 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '<h1> Hello, World!</h1>'
+
+@app.route('/data')
+def data():
+    return '<h1> DATA E COOLT</h1>'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
