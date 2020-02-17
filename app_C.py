3fde1166a8cd6fa285d003fa4dff15e74c7150e0
Christian Adamsson <Christian.liu171@gmail.com>
2020-02-17 16:29:49 +0100

    added replay files and stuff

--- app_C.py
+++ app_C.py
@@ -6,11 +6,12 @@ import numpy as np
 import json
 import sys
 
+## todo, should fix better handling of allmatches arrays, taking mean and variance directly from them. Structuring up the splitting of data and filtering directly.
 from flask import Flask, render_template
 from flask import request
 app = Flask(__name__)
 
-numberofmatches = 2
+numberofmatches = 10
 allmatches = {}
 allmatches_CK = {}
 allmatches_CK_wins = {}
@@ -55,9 +56,9 @@ def create_plot2():
 
         print('test output 2: ' + str(sneaky['percentage supersonic speed'].values) + 'kuzon: ' + str(kuzon['percentage supersonic speed'].values))
         
-        fig = go.Figure(data = [
+        fig1 = go.Figure(data = [
         go.Bar(
-            name = "sneaky" + str(1),
+            name = "sneaky",
             x = xdata,
             #y=[18, 15, 20]
             y=[int(sneaky['percentage supersonic speed'].values), int(sneaky['percentage boost speed'].values), int(sneaky['percentage slow speed'].values)]
@@ -74,10 +75,56 @@ def create_plot2():
             y = [int(kuzon['percentage supersonic speed'].values), int(kuzon['percentage boost speed'].values), int(kuzon['percentage slow speed'].values)]
             )
         ])
-        fig.update_layout(barmode='group')
-        graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)   
+        fig1.update_layout(barmode='group')
+        graphJSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)   
+
+        xdata2 = ['percentage defensive third', 'percentage neutral third', 'percentage offensive third']
+        fig2 = go.Figure(data = [
+        go.Bar(
+            name = "sneaky",
+            x = xdata2,
+            #y=[18, 15, 20]
+            y=[int(sneaky['percentage defensive third'].values), int(sneaky['percentage neutral third'].values), int(sneaky['percentage offensive third'].values)]
+            #y = [int(sneaky['percentage supersonic speed'].values), 15, 20]
+            #x=allmatches_CK_wins[0]['player name', 'percentage boost speed'], # assign x as the dataframe column 'x'
+            #x = xdata,
+            
+            
+        ),
+        go.Bar(
+            name = "kuzon",
+            x = xdata2,
+            #y=[18, 15, 20]
+            y = [int(kuzon['percentage defensive third'].values), int(kuzon['percentage neutral third'].values), int(kuzon['percentage offensive third'].values)]
+            )
+        ])
+        fig2.update_layout(barmode='group')
+        graphJSON2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)   
+
+        xdata3 = ['percentage on ground', 'percentage low in air', 'percentage high in air']
+        fig3 = go.Figure(data = [
+        go.Bar(
+            name = "sneaky",
+            x = xdata3,
+            #y=[18, 15, 20]
+            y=[int(sneaky['percentage on ground'].values), int(sneaky['percentage low in air'].values), int(sneaky['percentage high in air'].values)]
+            #y = [int(sneaky['percentage supersonic speed'].values), 15, 20]
+            #x=allmatches_CK_wins[0]['player name', 'percentage boost speed'], # assign x as the dataframe column 'x'
+            #x = xdata,
+            
+            
+        ),
+        go.Bar(
+            name = "kuzon",
+            x = xdata3,
+            #y=[18, 15, 20]
+            y = [int(kuzon['percentage on ground'].values), int(kuzon['percentage low in air'].values), int(kuzon['percentage high in air'].values)]
+            )
+        ])
+        fig3.update_layout(barmode='group')
+        graphJSON3 = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)   
         
-        return render_template('index_C.html', plot=graphJSON)
+        return render_template('index_C.html', plot1=graphJSON, plot2=graphJSON2, plot3=graphJSON3)
         #return graphJSON 
     elif "losses" in request.form:
         data = [
@@ -87,7 +134,7 @@ def create_plot2():
         )
         ]
         graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)   
-        return render_template('index_C.html', plot=graphJSON)
+        return render_template('index_C.html', plot1=graphJSON)
         #return graphJSON
     else:
         data = [
@@ -99,7 +146,7 @@ def create_plot2():
         )
         ]
         graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)   
-        return render_template('index_C.html', plot=graphJSON)
+        return render_template('index_C.html', plot1=graphJSON)
 
 def create_plot():
     N = 40
@@ -206,6 +253,7 @@ def read_data():
     #end of forloop
     #allmatches_CK_losses.reset_index
     #allmatches_CK_wins.reset_index
+    #sneakytotalscore = mean[allmatches_CK['player name'] == sn]
     sneakyaveragespeed = average(sneakytotalspeed, numberofmatches)
     kuzonaveragespeed = average(kuzontotalspeed, numberofmatches)
     Sneakyaveragescore = average(sneakytotalscore, numberofmatches)
